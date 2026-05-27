a95182d8506364b03a67b96121ccf8a76654f9af
Ravel Tanjaya <rtanjaya@freelancer.com>
2026-05-27 15:32:45 +1000

    feat: overseer focus and float

--- lua/plugins/workflow.lua
+++ lua/plugins/workflow.lua
@@ -7,22 +7,61 @@ return {
       { "<leader>or", "<cmd>OverseerRun<cr>", desc = "Run task" },
       { "<leader>oc", "<cmd>OverseerShell<cr>", desc = "Run shell command" },
       { "<leader>ot", "<cmd>OverseerToggle<cr>", desc = "Toggle task list" },
-      { "<leader>ol", "<cmd>OverseerTaskAction<cr>", desc = "Task action" },
+      {
+        "<leader>ol",
+        function()
+          local task_list = require("overseer.task_list")
+          local action_util = require("overseer.action_util")
+          local tasks = task_list.list_tasks({
+            unique = true,
+            sort = task_list.sort_finished_recently,
+            include_ephemeral = true,
+          })
+          if #tasks == 0 then
+            vim.notify("No tasks available", vim.log.levels.WARN)
+            return
+          end
+          vim.ui.select(tasks, {
+            prompt = "Open task (float)",
+            kind = "overseer_task",
+            format_item = function(t)
+              return t.name
+            end,
+          }, function(task)
+            if task then
+              action_util.run_task_action(task, "open float")
+            end
+          end)
+        end,
+        desc = "Open task in float",
+      },
     },
-    opts = function()
+    config = function()
       local disable_template_modules = {}
       if not vim.g.gaf then
         table.insert(disable_template_modules, "^overseer%.template%.user%.")
       end
-      return {
+      require("overseer").setup({
         dap = true,
         disable_template_modules = disable_template_modules,
         task_list = {
           direction = "bottom",
           min_height = 8,
           max_height = { 20, 0.2 },
+          keymaps = {
+            ["<CR>"] = { "keymap.open", opts = { dir = "float" }, desc = "Open task output in float" },
+            ["o"] = { "keymap.open", opts = { dir = "float" }, desc = "Open task output in float" },
+          },
         },
-      }
+        component_aliases = {
+          default = {
+            { "open_output", on_start = "always", direction = "float", focus = true },
+            "on_exit_set_status",
+            "on_complete_notify",
+            { "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
+          },
+        },
+      })
     end,
   },
 
